21e04b8381e966d549ed00a4bf9b77c6d2315da2
gustavo <gustavo.cesariosilva15@gmail.com>
2024-03-10 11:46:49 -0300

    Put and Delete

--- src/main/kotlin/com/bardavid/springkotlin/Controllers/ProductController.kt
+++ src/main/kotlin/com/bardavid/springkotlin/Controllers/ProductController.kt
@@ -3,11 +3,15 @@ package com.bardavid.springkotlin.Controllers
 import com.bardavid.springkotlin.Models.Product
 import com.bardavid.springkotlin.Repositories.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("products")
@@ -23,5 +27,24 @@ class ProductController {
     @PostMapping
     fun add (@RequestBody product: Product) : Product{
         return productRepository.save(product)
+    }
+
+    @DeleteMapping("{id}")
+    fun del(@PathVariable id: Long) {
+        return productRepository.deleteById(id)
+    }
+
+    @GetMapping("{id}")
+    fun find(@PathVariable id: Long): Optional<Product> {
+        return productRepository.findById(id)
+    }
+
+    @PutMapping("{id}")
+    fun alter (@PathVariable id: Long, @RequestBody product: Product) : Product{
+        if (productRepository.existsById(id)){
+            val safeProduct = product.copy(id)
+            return productRepository.save(safeProduct)
+        }
+        return Product()
     }
 }
--- src/main/kotlin/com/bardavid/springkotlin/Models/Product.kt
+++ src/main/kotlin/com/bardavid/springkotlin/Models/Product.kt
@@ -1,12 +1,14 @@
 package com.bardavid.springkotlin.Models
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 
 @Entity
-class Product(@Id
+data class Product(@Id
         @GeneratedValue
+              @JsonProperty(value = "id", access = JsonProperty.Access.READ_ONLY)
         val id: Long = 0L,
         val name: String = "",
         val price: String = "")
